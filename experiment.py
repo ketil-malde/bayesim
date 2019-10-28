a007009160cbf715cf9f5cf65fb9f79f61a0f3df
Ketil Malde <ketil@malde.org>
2019-10-28 11:23:32 +0100

    Fix NaNs in correlation (set to zero), add graphical output

--- experiment.py
+++ experiment.py
@@ -25,13 +25,28 @@ def correlate(data):
 
     # Calculate correlations r_{i,j}
     for i in range(m):
+        stdev_i = sqrt(n*sx2[i]-sx[i]**2)
         for j in range(m):
-            res[i,j] = (n*res[i,j]-sx[i]*sx[j]) / (sqrt(n*sx2[i]-sx[i]**2) * sqrt(n*sx2[j]-sx[j]**2))
-    print(res)
+            stdev_j = sqrt(n*sx2[j]-sx[j]**2)
+            if stdev_i != 0 and stdev_j != 0:
+                res[i,j] = (n*res[i,j]-sx[i]*sx[j]) / (stdev_i*stdev_j)
+            else:
+                res[i,j] = 0
+    return res
 
 # Testing:    
 # correlate(np.array([[1,0],[0,1],[1,1]]))
-            
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+x = np.array(b.iterate(b.mklayerednet(10,[(10,2),(10,3),(10,3)])))
+a = correlate(x)
+sns.heatmap(a[:10], cmap='coolwarm', linewidth=0.5)
+# plt.xtics([9.5,19.5]) how do I do this?
+plt.grid(True)
+plt.show()
+
 # for n networks of given complexity
 #   run network x times to generate data
 #   learn output using {SVM, RF, KNN, ANN} (how to optimize parameters?)
